1f17255559152ade85e021ac894c18ab8db21473
Huy Hoang <alteracctive@Huys-MacBook-Pro.local>
2025-01-25 20:40:52 -0600

    implementing headers

---
+++ tamuhack25/app/home/page.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function DashboardLayout({ children }) {
+    return (
+      <html lang="en">
+        <body>
+          {/* Layout UI */}
+          {/* Place children where you want to render a page or nested layout */}
+          <main>{children}</main>
+        </body>
+      </html>
+    )
+  }
