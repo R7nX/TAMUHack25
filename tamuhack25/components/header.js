c4721877a13420c410a07ac4b90da01fcaf11dac
baoph <baokhanh0514@gmail.com>
2025-01-25 22:43:20 -0700

    Fix <a> inside of <Link> bug

--- tamuhack25/components/header.js
+++ tamuhack25/components/header.js
@@ -1,22 +1,18 @@
+'use client';
+
 import Link from 'next/link'
 
-const Header = () =>{
-    return (
-        <header className="bg-gray-800 text-white py-4 px-8 flex justify-between items-center">
+const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white py-4 px-8 flex justify-between items-center">
       <h1 className="text-xl font-bold">My App</h1>
       <nav className="flex space-x-4">
-        <Link href="/home">
-          <a className="hover:underline">Home</a>
-        </Link>
-        <Link href="/budget">
-          <a className="hover:underline">Budget</a>
-        </Link>
-        <Link href="/market">
-          <a className="hover:underline">Market</a>
-        </Link>
+        <Link href="/home" className="hover:underline">Home</Link>
+        <Link href="/budget" className="hover:underline">Budget</Link>
+        <Link href="/market" className="hover:underline">Market</Link>
       </nav>
     </header>
-    );
+  );
 };
 
 export default Header;
